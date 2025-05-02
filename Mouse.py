2ace8b50cf4d62601d88487459be52a9eae871b7
Gabriel Leoncio Verdi <101580365+glVerdi@users.noreply.github.com>
2025-05-02 15:59:54 -0300

    Criação da classe Mouse.py

---
+++ Mouse.py
@@ -0,0 +1,20 @@
+class Mouse:
+  def __init__ (self):
+      self.__cor = cor
+      self.__qtd_botoes = qtd_botoes
+
+  @property
+  def cor(self):
+    return sel.__cor
+
+  @cor.setter
+  def cor(self, nova_cor):
+    self.__cor = nova_cor
+
+  @property
+  def qnt_bbotoes(self):
+    return self.__qtd_botoes
+
+  @qtd_botoes.setter
+  de qtd_botoes(self, nova_qtd):
+  self.__qtd_botoes = nova_qtd
